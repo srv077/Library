fef9e7a5ff77df66a80c8c65efb91b0a81ddc20e
srv077 <105067288+srv077@users.noreply.github.com>
2022-10-10 19:05:46 +0530

    third edit

--- index.js
+++ index.js
@@ -59,5 +59,72 @@ function date(){
 time();
 function time(){
     let now=new Date();
-    document.getElementById("time").innerHTML=now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+    let hours=now.getHours();
+    let min=now.getMinutes();
+    let sec=now.getSeconds();
+    document.getElementById("time").innerHTML=hours+":"+min+":"+sec;
+    setTimeout("time()", 1000);
+}
+function showcard(num){
+    document.getElementById("fullpage").style.filter="blur(5px)";
+    document.getElementById("card").style.display="block";
+    let slides=document.getElementsByClassName("authorimage");
+    for(let i=0; i<slides.length; i++) {
+        slides[i].style.display="none";
+    }
+    let books=document.getElementsByClassName("bookimage");
+    for(let i=0; i<books.length; i++) {
+        books[i].style.display="none";
+    }
+    let para=document.getElementsByClassName("para");
+    for(let i=0; i<para.length; i++) {
+        para[i].style.display="none";
+    }
+    if(num==-1){
+        document.getElementById("author-1").style.display="block";
+        document.getElementById("book-1").style.display="block";
+        document.getElementById("para-1").style.display="block";
+    }else if(num==1){
+        document.getElementById("author1").style.display="block";
+        document.getElementById("book1").style.display="block";
+        document.getElementById("para1").style.display="block";
+    }else if(num==-2){
+        document.getElementById("author-2").style.display="block";
+        document.getElementById("book-2").style.display="block";
+        document.getElementById("para-2").style.display="block";
+    }else if(num==2){
+        document.getElementById("author2").style.display="block";
+        document.getElementById("book2").style.display="block";
+        document.getElementById("para2").style.display="block";
+    }else if(num==-3){
+        document.getElementById("author-3").style.display="block";
+        document.getElementById("book-3").style.display="block";
+        document.getElementById("para-3").style.display="block";
+    }else if(num==3){
+        document.getElementById("author3").style.display="block";
+        document.getElementById("book3").style.display="block";
+        document.getElementById("para3").style.display="block";
+    }else if(num==-4){
+        document.getElementById("author-4").style.display="block";
+        document.getElementById("book-4").style.display="block";
+        document.getElementById("para-4").style.display="block";
+    }else if(num==4){
+        document.getElementById("author4").style.display="block";
+        document.getElementById("book4").style.display="block";
+        document.getElementById("para4").style.display="block";
+    }else if(num==-5){
+        document.getElementById("author-5").style.display="block";
+        document.getElementById("book-5").style.display="block";
+        document.getElementById("para-5").style.display="block";
+    }else if(num==5){
+        document.getElementById("author5").style.display="block";
+        document.getElementById("book5").style.display="block";
+        document.getElementById("para5").style.display="block";
+    }
+}
+function closecard(){
+    document.getElementById("card").style.display="none";
+    document.getElementById("fullpage").style.filter="blur(0px)";
+    document.getElementById("classic-mover-prev").style.top="130vh";
+    document.getElementById("classic-mover-next").style.top="130vh";
 }
